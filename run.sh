4ae0b67fe3d210dcd99c7f642aacf9bd2efc7653
diarmogalla <diarmuid.gallagher@kainos.com>
2025-08-20 12:56:28 +0100

    Fixing run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 
 echo "Installing dependencies..."
 pip3 install -r requirements.txt --quiet --disable-pip-version-check
 
 echo "Starting Pomodoro App..."
-python3 pomodoro.py
+python3 main.py
